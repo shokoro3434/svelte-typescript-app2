e1b379a3a3825988003d7fd5856faa27fb9f1a3a
ishii takahiro <nonamennm03@gmail.com>
2023-11-29 08:35:58 +0900

    svelte

--- src/components/MountWithFetch.svelte
+++ src/components/MountWithFetch.svelte
@@ -1,47 +1,62 @@
 <script lang="ts">
-    import { fetchTodos,postDefenses } from "../hooks/fetchDefenses";
-    import { todos } from "../stores/ToDo";
-
-    fetchTodos();
-    postDefenses();
+  import { todos } from "../stores/ToDo";
+  import { defenses } from "../stores/Defense";
+  import { fetchDefenses } from "../hooks/DefensesHook.svelte";
+  import { fetchTodos } from "../hooks/ToDoHook.svelte";
+  fetchDefenses();
+  fetchTodos();
 </script>
 
 <h2>onMountの例</h2>
 <ul>
-    <div>
-        {#each $todos as todo}
-            <li>
-                {todo.id}
-                {todo.title}
-                <input type="checkbox" checked={todo.completed} />
-            </li>
-        {:else}
-            <div>Loading...</div>
-        {/each}
-    </div>
+  <div>
+    {#each $todos as todo}
+      <li>
+        {todo.id}
+        {todo.title}
+        <input type="checkbox" checked={todo.completed} />
+      </li>
+    {:else}
+      <div>Loading...</div>
+    {/each}
+  </div>
 </ul>
+<h2>受けプレイ</h2>
+<ul>
+  <div>
+    {#each $defenses as rec}
+      <li>
+        { rec.properties.id.number }
+        { rec.properties.name.rich_text[0].plain_text }
+      </li>
+    {:else}
+      <div>Loading...</div>
+    {/each}
+  </div>
+</ul>
+
 <style>
-    ul {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      color: #fff;
-    }
-    li {
-      text-align: left;
-      list-style: none;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: 1rem 2rem;
-    }
-    li:nth-child(2n + 1) {
-      background-color: #6998ab;
-    }
-    li:nth-child(2n) {
-      background-color: #406882;
-    }
-    input {
-      margin: 0 1rem;
-    }
-  </style>
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #fff;
+  }
+  li {
+    text-align: left;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 1rem 2rem;
+  }
+  li:nth-child(2n + 1) {
+    background-color: #6998ab;
+  }
+  li:nth-child(2n) {
+    background-color: #406882;
+  }
+  input {
+    margin: 0 1rem;
+  }
+</style>
--- src/stores/Defense.ts
+++ src/stores/Defense.ts
@@ -1,8 +1,40 @@
-import { writable,readable } from "svelte/store";
-
-class Defense{
+import { writable, readable } from "svelte/store";
+/*
+class Results{
+    properties:Properties;
+}
+class Properties{
     id:number;
     name:string;
 }
-export const defences = writable<Defense[]>([]);
+class Id{
+    number:number;
+}
+class Name{
+
+}
+class rich_text
+class Defense{
+    object:string;
+    results:Results[];
+}
+*/
+const results = [
+    {
+        properties: {
+            id: {
+                "number": 0
+            },
+            name: {
+                rich_text:[
+                    {
+                        plain_text: '',
+                    }
+                ],
+            }
+        }
+    }
+]
+
+export const defenses = writable(results);
 
